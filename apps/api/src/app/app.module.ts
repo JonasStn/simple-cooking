00743dc8744107a2419d5fe2e1494f4bcb1c923b
jonasStn <jonas.stein86@hotmail.de>
2020-02-02 19:23:07 +0100

    build(simple-cooking-web): add db connection

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 
@@ -7,9 +9,16 @@ import { AppService } from './app.service';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.dev.env'
+    }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'simple-cooking-web')
-    })
+    }),
+    MongooseModule.forRoot(
+      process.env.PROD_MONGODB || process.env.DEV_MONGODB,
+      { useNewUrlParser: true, useUnifiedTopology: true }
+    )
   ],
   controllers: [AppController],
   providers: [AppService]
