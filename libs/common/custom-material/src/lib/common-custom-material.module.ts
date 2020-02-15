162c90d10615b05538327defe1cce8dd0df85c90
jonasStn <jonas.stein86@hotmail.de>
2020-02-16 00:08:27 +0100

    feat(common/custom-material): add custom icon set

--- libs/common/custom-material/src/lib/common-custom-material.module.ts
+++ libs/common/custom-material/src/lib/common-custom-material.module.ts
@@ -7,21 +7,28 @@ import { MatInputModule } from '@angular/material/input';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { DomSanitizer } from '@angular/platform-browser';
 
-
 @NgModule({
-  imports: [MatProgressBarModule,MatIconModule, MatButtonModule,MatFormFieldModule, MatInputModule, MatCardModule],
-  exports: [MatProgressBarModule,MatIconModule, MatButtonModule,MatFormFieldModule, MatInputModule, MatCardModule]
+  imports: [
+    MatProgressBarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule
+  ],
+  exports: [
+    MatProgressBarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule
+  ]
 })
 export class CommonCustomMaterialModule {
-  constructor(
-    iconRegistry: MatIconRegistry,
-    private domSanitizer: DomSanitizer
-  ) {
-    iconRegistry.addSvgIcon(
-      'google-logo',
-      this.domSanitizer.bypassSecurityTrustResourceUrl(
-        'https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg'
-      )
+  constructor(iconRegistry: MatIconRegistry, domSanitizer: DomSanitizer) {
+    iconRegistry.addSvgIconSet(
+      domSanitizer.bypassSecurityTrustResourceUrl('assets/custom-icons.svg')
     );
   }
 }
