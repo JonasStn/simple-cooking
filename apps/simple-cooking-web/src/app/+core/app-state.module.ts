9692ba5db7170a61d014344b46ad62da2711d594
jonasStn <jonas.stein86@hotmail.de>
2020-02-16 14:25:32 +0100

    feat(simple-cooking-web): add APP_INITIALIZER for loading user

--- apps/simple-cooking-web/src/app/+core/app-state.module.ts
+++ apps/simple-cooking-web/src/app/+core/app-state.module.ts
@@ -4,11 +4,14 @@ import { EffectsModule } from '@ngrx/effects';
 import {
   MinimalRouterStateSerializer,
   NavigationActionTiming,
-  StoreRouterConnectingModule
+  StoreRouterConnectingModule,
 } from '@ngrx/router-store';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '@simple-cooking/env/environment';
+import {
+  SharedDataAccessAuthModule,
+} from '@simple-cooking/shared/data-access-auth';
 
 @NgModule({
   declarations: [],
@@ -29,7 +32,8 @@ import { environment } from '@simple-cooking/env/environment';
     StoreRouterConnectingModule.forRoot({
       navigationActionTiming: NavigationActionTiming.PostActivation,
       serializer: MinimalRouterStateSerializer
-    })
+    }),
+    SharedDataAccessAuthModule
   ]
 })
 export class AppStateModule {}
--- apps/simple-cooking-web/src/app/+core/core.module.ts
+++ apps/simple-cooking-web/src/app/+core/core.module.ts
@@ -1,9 +1,23 @@
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { AuthFacade } from '@simple-cooking/shared/data-access-auth';
 
 import { AppStateModule } from './app-state.module';
 
+const appInitializer = (authFacade: AuthFacade) => {
+  return () => {
+    authFacade.loadUser();
+  };
+};
 @NgModule({
-  imports: [HttpClientModule, AppStateModule]
+  imports: [HttpClientModule, AppStateModule],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: appInitializer,
+      deps: [AuthFacade],
+      multi: true
+    }
+  ]
 })
 export class CoreModule {}
