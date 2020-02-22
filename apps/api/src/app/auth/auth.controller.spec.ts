04cb21e6781809261fffe20da834554068d05e1a
jonasStn <jonas.stein86@hotmail.de>
2020-02-22 18:48:44 +0100

    feat(api): fix tests

--- apps/api/src/app/auth/auth.controller.spec.ts
+++ apps/api/src/app/auth/auth.controller.spec.ts
@@ -1,15 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { createSpyObject, SpyObject } from '@ngneat/spectator/jest';
+
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
 describe('Auth Controller', () => {
   let controller: AuthController;
+  let authService: SpyObject<AuthService>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [AuthController]
+      controllers: [AuthController],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: createSpyObject(AuthService)
+        }
+      ]
     }).compile();
-
     controller = module.get<AuthController>(AuthController);
+    authService = module.get(AuthService) as SpyObject<AuthService>;
   });
 
   it('should be defined', () => {
--- apps/api/src/app/auth/auth.service.spec.ts
+++ apps/api/src/app/auth/auth.service.spec.ts
@@ -1,15 +1,28 @@
+import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
+import { createSpyObject } from '@ngneat/spectator/jest';
+import { Model } from 'mongoose';
+
 import { AuthService } from './auth.service';
+import { UserInfo } from './interfaces/user-info.interface';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let repository: Model<UserInfo>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService]
+      providers: [
+        AuthService,
+        {
+          provide: getModelToken('UserInfo'),
+          useValue: createSpyObject(Model)
+        }
+      ]
     }).compile();
 
     service = module.get<AuthService>(AuthService);
+    repository = module.get<Model<UserInfo>>(getModelToken('UserInfo'));
   });
 
   it('should be defined', () => {
