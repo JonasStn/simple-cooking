4b8f4c46722780689c567148844dd5d245d331a5
jonasStn <jonas.stein86@hotmail.de>
2020-02-22 18:35:08 +0100

    feat(dashboard): fix default test

--- libs/feature-dashboard/src/lib/dashboard-page/dashboard-page.component.spec.ts
+++ libs/feature-dashboard/src/lib/dashboard-page/dashboard-page.component.spec.ts
@@ -1,15 +1,22 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { createSpyObject, SpyObject } from '@ngneat/spectator/jest';
+import { AuthFacade } from '@simple-cooking/shared/data-access-auth';
 
 import { DashboardPageComponent } from './dashboard-page.component';
 
 describe('DashboardPageComponent', () => {
   let component: DashboardPageComponent;
   let fixture: ComponentFixture<DashboardPageComponent>;
+  let authFacade: SpyObject<AuthFacade>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [DashboardPageComponent]
+      declarations: [DashboardPageComponent],
+      providers: [
+        { provide: AuthFacade, useValue: createSpyObject(AuthFacade) }
+      ]
     }).compileComponents();
+    authFacade = TestBed.inject(AuthFacade) as SpyObject<AuthFacade>;
   }));
 
   beforeEach(() => {
