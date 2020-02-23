2f4dd080322f26803aaa8dadffdb2c1fbdd78878
jonasStn <jonas.stein86@hotmail.de>
2020-02-23 20:01:57 +0100

    feat(api): add tests

--- apps/api/src/app/auth/auth.controller.spec.ts
+++ apps/api/src/app/auth/auth.controller.spec.ts
@@ -1,8 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { createSpyObject, SpyObject } from '@ngneat/spectator/jest';
+import { UserInfoDTO } from '@simple-cooking/api-interfaces';
+import { Request, Response } from 'express';
 
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { UserInfo } from './interfaces/user-info.interface';
 
 describe('Auth Controller', () => {
   let controller: AuthController;
@@ -24,5 +27,48 @@ describe('Auth Controller', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should redirect with token', () => {
+    const res = { redirect: jest.fn() };
+    const jwt = 'token';
+    const req = { user: { jwt } };
+
+    controller.googleLoginCallback(
+      req as Request & { user: { jwt: string } },
+      (res as unknown) as Response
+    );
+    expect(res.redirect).toHaveBeenCalledWith(
+      `http://localhost:4200/login?_token=${jwt}`
+    );
+  });
+
+  it('should redirect to login/failure', () => {
+    const res = { redirect: jest.fn() };
+    const jwt = null;
+    const req = { user: { jwt } };
+
+    controller.googleLoginCallback(
+      req as Request & { user: { jwt: string } },
+      (res as unknown) as Response
+    );
+    expect(res.redirect).toHaveBeenCalledWith(
+      'http://localhost:4200/login/failure'
+    );
+  });
+
+  it('should fetch user', async () => {
+    const expected: UserInfoDTO = {
+      familyName: 'Doe',
+      givenName: 'John',
+      pictureUrl: 'www.some-picture.url',
+      userId: '123'
+    };
+    authService.findOne.mockReturnValue(
+      new Promise((resolve, reject) => resolve(expected as UserInfo))
+    );
+    const user = await controller.fetchUser('123');
+    expect(authService.findOne).toHaveBeenCalledWith('123');
+    expect(user).toEqual(expected);
   });
 });
--- apps/api/src/app/auth/auth.service.spec.ts
+++ apps/api/src/app/auth/auth.service.spec.ts
@@ -2,9 +2,26 @@ import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
 import { createSpyObject } from '@ngneat/spectator/jest';
 import { Model } from 'mongoose';
+import { Profile } from 'passport-google-oauth20';
 
-import { AuthService } from './auth.service';
+import { AuthService, Provider } from './auth.service';
 import { UserInfo } from './interfaces/user-info.interface';
+
+const profile: Profile = {
+  id: '123',
+  name: {
+    familyName: 'Doe',
+    givenName: 'John'
+  },
+  photos: [{ value: 'www.some-picture.url' }]
+} as Profile;
+
+const user = {
+  userId: '123',
+  givenName: 'John',
+  familyName: 'Doe',
+  pictureUrl: 'www.some-picture.url'
+};
 
 describe('AuthService', () => {
   let service: AuthService;
@@ -27,5 +44,25 @@ describe('AuthService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('validateOAuthLogin', () => {
+    it('should create user', async () => {
+      service.findOne = jest.fn(
+        () => new Promise((resolve, _reject) => resolve(null))
+      );
+      service.createUser = jest.fn();
+      await service.validateOAuthLogin(profile, Provider.GOOGLE);
+      expect(service.createUser).toHaveBeenCalledWith(user);
+    });
+
+    it('should not override existing user', async () => {
+      service.findOne = jest.fn(
+        () => new Promise((resolve, _reject) => resolve(user as UserInfo))
+      );
+      service.createUser = jest.fn();
+      await service.validateOAuthLogin(profile, Provider.GOOGLE);
+      expect(service.createUser).not.toHaveBeenCalled();
+    });
   });
 });
